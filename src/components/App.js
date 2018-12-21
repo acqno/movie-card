004ba65f43b8fecea33cfddaed51a8bd8fb2c67b
Alvin Quijano <alvin.quijano@live.ca>
2018-12-21 13:49:25 -0500

    added request to find movie poster path

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import MovieDetail from './MovieDetail';
 import moviedb from '../apis/moviedb';
 
 class App extends React.Component {
-	state = { movie: [] };
+	state = { movie: [], img_path: '' };
 
 	// this call back method takes the user input search title and searches the moviedb api 
 	// it returns the data in a .json object that we store in variable response
@@ -16,19 +16,25 @@ class App extends React.Component {
 		});
 
 		this.setState({ movie: response.data.results[0] });
-		console.log(this.state.movie);
+
+		// finds the movie poster path of the searched movie
+		const config = await moviedb.get('/configuration');
+		const posterPath = config.data.images.base_url + '/original' + this.state.movie.poster_path;
+		
+		this.setState({ img_path: posterPath });
+		//console.log(this.state.img_path);
 	};
 
 	render() {
 		return (
-			<div className="uk-container uk-container-large">
-
-				<div className="movie-details__container">
-					<MovieDetail movie={this.state.movie} />
-				</div>
+			<div className="uk-container uk-container-large movie-card">
 				<div className="movie-poster__container">
 
 				</div>
+				<div className="movie-details__container">
+					<MovieDetail movie={this.state.movie} />
+				</div>
+				
 				<SearchBar onTitleSubmit={this.onTitleSubmit} />
 			</div>
 		);
