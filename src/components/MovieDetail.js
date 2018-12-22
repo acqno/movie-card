fc0c5445a4b00014c4d4381834585387e7d791f5
Alvin Quijano <alvin.quijano@live.ca>
2018-12-22 02:30:35 -0500

    refactored to add destructuring

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -2,13 +2,13 @@
 
 import React from 'react';
 
-const MovieDetail = (props) => {
+const MovieDetail = ({ movie }) => {
 
     return (
         <div className="movie-details">
-            <h1 className="movie-title">{props.movie.title}</h1>
-            <h3 className="movie-release">{props.movie.release_date}</h3>
-            <p className="movie-overview">{props.movie.overview}</p>
+            <h1 className="movie-title">{movie.title}</h1>
+            <h3 className="movie-release">{movie.release_date}</h3>
+            <p className="movie-overview">{movie.overview}</p>
         </div>
     );
 }
